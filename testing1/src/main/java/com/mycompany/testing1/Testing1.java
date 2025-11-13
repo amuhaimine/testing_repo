ce0238a1dc63dd27cb2f1283668c6da355f71753
muhaimin_pc <>
2025-11-13 16:13:00 +0800

    add new file for starter

---
+++ testing1/src/main/java/com/mycompany/testing1/Testing1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testing1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Testing1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
